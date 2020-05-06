a6c1a96e5f96bcc5f1d92b3bd9f6181a74805bd7
ehmicky <ehmicky@gmail.com>
2020-05-06 16:06:39 +0200

    Improve error handling

--- index.js
+++ index.js
@@ -11,14 +11,22 @@ const getHtmlFiles = async (directory) => {
   return files.filter((file) => path.extname(file) === '.html')
 }
 
+const inlineSources = async ({ htmlFiles, inputs, constants, utils }) => {
+  try {
+    const inlineSourcePromises = htmlFiles.map((file) =>
+      inlineSource(file, { ...inputs, rootpath: constants.PUBLISH_DIR })
+    )
+    const htmlWithInlinedSources = await Promise.all(inlineSourcePromises)
+    return htmlWithInlinedSources
+  } catch (error) {
+    utils.build.failBuild('Inlining sources failed.', { error })
+  }
+}
+
 module.exports = {
   onPostBuild: async ({ inputs, constants, utils }) => {
-    try {
       const htmlFiles = await getHtmlFiles(constants.PUBLISH_DIR)
-      const inlineSourcePromises = htmlFiles.map((file) =>
-        inlineSource(file, { ...inputs, rootpath: constants.PUBLISH_DIR })
-      )
-      const htmlWithInlinedSources = await Promise.all(inlineSourcePromises)
+      const htmlWithInlinedSources = await inlineSources({ htmlFiles, inputs, constants, utils })
       const overwriteFilePromises = htmlWithInlinedSources.map(
         (content, index) => {
           const filePath = path.join(constants.PUBLISH_DIR, htmlFiles[index])
@@ -30,8 +38,5 @@ module.exports = {
       await Promise.all(overwriteFilePromises)
 
       console.log('Sources successfully inlined!')
-    } catch (error) {
-      utils.build.failBuild('Inlining sources failed.', { error })
-    }
   }
 }
